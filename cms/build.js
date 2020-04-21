0f73b3b9049d9eac139b7c09602d99cd073cf2c3
Emmanuel Gratuze <emmanuel.gratuze@gmail.com>
2020-04-21 13:28:56 +0200

    Fix build

--- cms/build.js
+++ cms/build.js
@@ -2,25 +2,25 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import YAML from 'yaml'
 import fs from 'fs'
-import configFile from '../cms/config'
+import configFile from './config'
 
 const yamlConfig = YAML.stringify(configFile)
 
-function createConfigFile(yamlConfigString) {
-  fs.writeFile('./public/admin/config.yml', yamlConfigString, () => {
+function createConfigFiles(yamlConfigString) {
+  fs.writeFile('./public/config.yml', yamlConfigString, () => {
     console.log('Netlify CMS config file created successfully.')
   })
 }
 
-if (!fs.existsSync('./public/admin')) {
+if (!fs.existsSync('./public')) {
   // eslint-disable-next-line prefer-numeric-literals
-  fs.mkdirSync('./public/admin', parseInt('0766', 8), (err) => {
+  fs.mkdirSync('./public', parseInt('0766', 8), (err) => {
     if (err) {
       console.log(err)
     } else {
-      createConfigFile(yamlConfig)
+      createConfigFiles(yamlConfig)
     }
   })
 } else {
-  createConfigFile(yamlConfig)
+  createConfigFiles(yamlConfig)
 }
--- utils/fetchFileContent.ts
+++ utils/fetchFileContent.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-const fetchFileContent = (url: string): Promise<string> => (
+const doFetch = (url: string): Promise<string> => (
   new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => {
@@ -11,5 +11,19 @@ const fetchFileContent = (url: string): Promise<string> => (
       })
   })
 )
+
+let lastNoSimultenousRequest: Promise<string> = new Promise((resolve) => { resolve() })
+
+const fetchFileContent = (url: string, noSimultaneous = false): Promise<string> => {
+  if (noSimultaneous) {
+    lastNoSimultenousRequest = lastNoSimultenousRequest
+      .then(() => (
+        doFetch(url)
+      ))
+    return lastNoSimultenousRequest
+  }
+
+  return doFetch(url)
+}
 
 export default fetchFileContent
